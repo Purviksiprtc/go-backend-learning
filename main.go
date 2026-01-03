d5ecc27a73dd674227779560998b896c85e441f6
Purvik Jawalkar <purvikj@siprtc.com>
2026-01-03 10:17:10 +0530

    3-1-26

--- main.go
+++ main.go
@@ -4,28 +4,34 @@ import (
 	"log"
 	"os"
 
-	"github.com/go-playground/validator/v10"
-
 	"user-crud-go/config"
 	"user-crud-go/models"
 	"user-crud-go/routes"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	// DB
 	config.ConnectDB()
 	config.DB.AutoMigrate(&models.User{})
 
+	// Echo
 	e := echo.New()
+
+	// ✅ Attach validator
 	e.Validator = &config.CustomValidator{
 		Validator: validator.New(),
 	}
+
+	// Routes
 	routes.RegisterRoutes(e)
 
+	// Port
 	port := os.Getenv("APP_PORT")
 	if port == "" {
-		port = "8080" // fallback safety
+		port = "8080"
 	}
 
 	if err := e.Start(":" + port); err != nil {
